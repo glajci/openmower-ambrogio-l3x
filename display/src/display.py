da6c930ae9a42c83f5332a0cac10750db906116e
glr <glajcar.g@eplan.pl>
2025-01-27 00:41:02 +0100

    NEW: LCD display added

---
+++ display/src/display.py
@@ -0,0 +1,132 @@
+from machine import I2C, Pin
+from pico_i2c_lcd import I2cLcd
+
+I2C_ADDR     = 39
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+
+class Display:
+
+    def __init__(self):
+        self.i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+        self.lcd = I2cLcd(self.i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+        self.define_custom_characters()
+        self.clear()
+        self.emergency = False
+
+    def clear(self):
+        self.isAsleep = True
+        self.lcd.clear()   
+
+    def draw_background(self):
+        self.isAsleep = False
+
+    def draw_splash(self):
+        self.lcd.move_to(0,0)
+        self.lcd.putstr("Openmower" + " " * (16 - len("Openmower")))
+
+    def draw_mower_state(self, state):
+        self.lcd.move_to(0,0)
+        self.lcd.putstr(state + " " * (15 - len(state)))
+
+    def draw_emergency(self, emergency: bool):
+        if emergency == self.emergency: return
+        self.emergency = emergency
+        
+        self.lcd.move_to(15,0)
+        if emergency:
+            self.lcd.putchar(chr(0))
+        else:
+            self.lcd.putstr(' ')
+
+    def draw_gps(self, accuracy, percentage):
+        self.lcd.move_to(0,1)
+        if accuracy <= 0.05:
+            self.lcd.putchar(chr(7))
+        elif accuracy <= 0.2:
+            self.lcd.putchar(chr(6))
+        else:
+            self.lcd.putchar(chr(5))
+
+        if accuracy > 1:
+            accuracy_formatted = f'{str(round(accuracy, 1))}m'
+        else:
+            accuracy_formatted = f'{str(round(accuracy, 3) * 100)}cm'
+
+        self.lcd.move_to(1,1)
+        self.lcd.putstr(accuracy_formatted + " " * (7 - len(accuracy_formatted)))
+
+    def draw_battery(self, percentage, is_charging):
+        char_index = int(percentage * 2) + 1
+
+        if is_charging:
+            char_index = 4
+
+        self.lcd.move_to(15,1)
+        self.lcd.putchar(chr(char_index))
+
+    def draw_battery_voltage(self, voltage):
+        voltage_formatted = f'{round(float(voltage.decode()), 1)}V'
+        self.lcd.move_to(8,1)
+        self.lcd.putstr(" " * (7 - len(voltage_formatted)) + voltage_formatted)
+
+    def draw_menu(self, menu_item):
+        if menu_item == None: return
+        self.lcd.move_to(0,1)
+        self.lcd.putstr(menu_item + " " * (16 - len(menu_item)))
+
+    def draw_print_message(self, message):
+        self.lcd.move_to(0,1)
+        self.lcd.putstr((message[:16]) + " " * (16 - len(message[:16])))
+                
+    def sleep(self):
+        self.lcd.backlight_off()
+        self.isAsleep = True     
+        
+    def wake(self):
+        self.lcd.backlight_on()
+        self.isAsleep = False
+
+    def define_custom_characters(self):
+    
+        # how to create customcharacters https://maxpromer.github.io/LCD-Character-Creator/ 
+  
+        #emergency      
+        self.lcd.custom_char(0, bytearray([
+            0xe,0xe,0xe,0xe,0xe,0x0,0xe,0xe
+        ]))
+
+        #battery empty
+        self.lcd.custom_char(1, bytearray([
+            0xe,0x11,0x11,0x11,0x11,0x11,0x11,0x1f
+        ]))
+  
+        #battery 1/6
+        self.lcd.custom_char(2, bytearray([
+            0xe,0x11,0x11,0x11,0x1f,0x1f,0x1f,0x1f
+        ]))
+  
+        #battery 6/6
+        self.lcd.custom_char(3, bytearray([
+            0xe,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f,0x1f
+        ]))
+  
+        #battery charging
+        self.lcd.custom_char(4, bytearray([
+            0xe,0x1f,0x1b,0x17,0x11,0x1d,0x1b,0x1f
+        ]))
+
+        #gps no rtk      
+        self.lcd.custom_char(5, bytearray([
+            0x0,0xe,0x19,0x15,0x13,0xe,0x0,0x0
+        ]))
+
+        #gps rtk float      
+        self.lcd.custom_char(6, bytearray([
+            0x0,0xe,0x11,0x11,0x11,0xe,0x0,0x0
+        ]))
+
+        #gps rtk fixed    
+        self.lcd.custom_char(7, bytearray([
+            0x0,0xe,0x1f,0x1b,0x1f,0xe,0x0,0x0
+        ]))
---
+++ display/src/openmower.py
@@ -0,0 +1,55 @@
+class Topic:
+    def __init__(self, name: str):
+        self.name = name
+        self.b_name = name.encode('utf-8') 
+
+class Topics:
+    def __init__(self):
+        self.actions = Topic("actions/json")
+        self.robot_state = Topic("robot_state/json")
+        self.v_battery = Topic("sensors/om_v_battery/data")
+
+    def get_all(self):
+        return [self.actions, self.robot_state, self.v_battery]
+
+class Action:
+    def __init__(self, id: str, name: str, enabled: bool):
+        self.id = id
+        self.name = name
+        self.enabled = enabled
+
+class Actions:
+    def __init__(self, json_actions):
+        self.reset_emergency = Action(None, None, False)
+        self.start_mowing = Action(None, None, False)
+        self.start_area_recording = Action(None, None, False)
+        self.pause_mowing = Action(None, None, False)
+        self.continue_mowing = Action(None, None, False)
+        self.abort_mowing = Action(None, None, False)
+        self.skip_area = Action(None, None, False)
+        self.skip_path = Action(None, None, False)
+
+        for json_action in json_actions:
+            if json_action['action_id'] == "mower_logic/reset_emergency":
+                self.reset_emergency = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:idle/start_mowing":
+                self.start_mowing = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:idle/start_area_recording":
+                self.start_area_recording = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:mowing/pause":
+                self.pause_mowing = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:mowing/continue":
+                self.continue_mowing = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:mowing/abort_mowing":
+                self.abort_mowing = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:mowing/skip_area":
+                self.skip_area = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+            elif json_action['action_id'] == "mower_logic:mowing/skip_path":
+                self.skip_path = Action(json_action['action_id'], json_action['action_name'], json_action['enabled'])
+
+class Openmower:
+    actions: Actions
+    topics = Topics()
+
+    def set_actions(json_actions):
+        Openmower.actions = Actions(json_actions)
